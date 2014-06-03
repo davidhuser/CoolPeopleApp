58a85bd36b2306c61fd4362e7c911ebeb237069d
David Huser <dafhus@gmail.com>
2014-06-03 17:21:19 +0200

    refactoring

--- src/main/java/ch/fhnw/coin/coolpeople/Config.java
+++ src/main/java/ch/fhnw/coin/coolpeople/Config.java
@@ -1,10 +1,15 @@
 package ch.fhnw.coin.coolpeople;
 
+import java.util.Arrays;
+import java.util.List;
+
 class Config {
 
-    //Input text resources
-    public static final String TXT1 = "/Users/david/Desktop/inputtext_eng.txt";
-    public static final String TXT2 = "/Users/david/Desktop/inputtext_eng2.txt";
+
+    public static final List<String> INPUT = Arrays.asList(
+            "/Users/david/Desktop/inputtext_eng.txt",
+            "/Users/david/Desktop/inputtext_eng2.txt"
+    );
 
     //Output file declaration. Keep extension: .gexf
     public static final String OUTPUTFILE = "/Users/david/Desktop/CoolPeopleApp_export.gexf";

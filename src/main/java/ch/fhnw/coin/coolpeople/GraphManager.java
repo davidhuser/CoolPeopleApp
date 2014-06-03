765647c111286a44f18c4602c53d88d7002f3c24
David Huser <dafhus@gmail.com>
2014-06-03 17:22:07 +0200

    skeleton for edges added

--- src/main/java/ch/fhnw/coin/coolpeople/GraphManager.java
+++ src/main/java/ch/fhnw/coin/coolpeople/GraphManager.java
@@ -1,75 +1,45 @@
 package ch.fhnw.coin.coolpeople;
 
-import it.uniroma1.dis.wsngroup.gexf4j.core.*;
-import it.uniroma1.dis.wsngroup.gexf4j.core.data.AttributeClass;
-import it.uniroma1.dis.wsngroup.gexf4j.core.data.AttributeList;
-import it.uniroma1.dis.wsngroup.gexf4j.core.impl.GexfImpl;
+import it.uniroma1.dis.wsngroup.gexf4j.core.Gexf;
+import it.uniroma1.dis.wsngroup.gexf4j.core.Graph;
+import it.uniroma1.dis.wsngroup.gexf4j.core.Node;
 import it.uniroma1.dis.wsngroup.gexf4j.core.impl.StaxGraphWriter;
-import it.uniroma1.dis.wsngroup.gexf4j.core.impl.data.AttributeListImpl;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 class GraphManager {
 
-    private final ArrayList<ArrayList<Person>> graph;
-
-    public GraphManager(ArrayList<ArrayList<Person>> al) {
-        this.graph = al;
-        initGraph(graph);
+    public GraphManager(ArrayList<ArrayList<Node>> al, Graph graph, Gexf gexf) {
+        ArrayList<ArrayList<Node>> nodelist = al;
+        Graph g = graph;
+        Gexf gx = gexf;
+        initGraph(al, g, gx);
     }
 
-    private void initGraph(ArrayList<ArrayList<Person>> al) {
+    private void initGraph(ArrayList<ArrayList<Node>> al, Graph g, Gexf gx) {
 
-        Gexf gexf = new GexfImpl();
-        Calendar date = Calendar.getInstance();
-
-        gexf.getMetadata()
-                .setLastModified(date.getTime())
-                .setCreator("CoolPeopleApp")
-                .setDescription("Document Network");
-        gexf.setVisualization(true);
-
-        Graph graph = gexf.getGraph();
-        graph.setDefaultEdgeType(EdgeType.UNDIRECTED).setMode(Mode.STATIC);
-
-        AttributeList attrList = new AttributeListImpl(AttributeClass.NODE);
-        graph.getAttributeLists().add(attrList);
-
-        //TODO links created nodes to edges depending on source document
-        int counter = 0;
-        ArrayList<Node> nodeList = new ArrayList<Node>();
-        for (ArrayList<Person> innerList : al) {
-            for (Person p : innerList) {
-                Node node = graph.createNode(Integer.toString(counter));
-                node.setLabel(p.toString());
-                nodeList.add(node);
-                connectNodeToAll(nodeList, node);
-                counter++;
-            }
-        }
+        //TODO create edges between nodes...
+        //https://github.com/francesco-ficarola/gexf4j/blob/master/src/examples/java/it/uniroma1/dis/wsngroup/gexf4j/examples/StaticGexfGraph.java
 
         StaxGraphWriter graphWriter = new StaxGraphWriter();
         File f = new File(Config.OUTPUTFILE);
         Writer out;
         try {
             out = new FileWriter(f, false);
-            graphWriter.writeToStream(gexf, out, "UTF-8");
-            System.out.println("GRAPH FILE CREATED AT:");
+            graphWriter.writeToStream(gx, out, "UTF-8");
             System.out.println(f.getAbsolutePath());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    private void connectNodeToAll(ArrayList<Node> list, Node node){
+    private void connectNodeToAll(ArrayList<Node> list, Node node) {
         int counter = 0;
-        for(Node p: list){
+        for (Node p : list) {
             counter++;
             node.connectTo(Integer.toString(counter), p);
         }

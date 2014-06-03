9dbab0b504245df6fa282e7be7bb83582d9134a9
David Huser <dafhus@gmail.com>
2014-06-03 17:27:23 +0200

    skeleton for edges added

--- src/main/java/ch/fhnw/coin/coolpeople/NameExtractor.java
+++ src/main/java/ch/fhnw/coin/coolpeople/NameExtractor.java
@@ -1,5 +1,10 @@
 package ch.fhnw.coin.coolpeople;
 
+import it.uniroma1.dis.wsngroup.gexf4j.core.*;
+import it.uniroma1.dis.wsngroup.gexf4j.core.data.AttributeClass;
+import it.uniroma1.dis.wsngroup.gexf4j.core.data.AttributeList;
+import it.uniroma1.dis.wsngroup.gexf4j.core.impl.GexfImpl;
+import it.uniroma1.dis.wsngroup.gexf4j.core.impl.data.AttributeListImpl;
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
 import opennlp.tools.sentdetect.SentenceDetectorME;
@@ -11,6 +16,7 @@ import opennlp.tools.util.Span;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * ch.fhnw.coin.coolpeople.NameExtractor is the class which tries to extract Names with two different approaches from an input String.
@@ -22,8 +28,11 @@ class NameExtractor {
 
 
     private final ArrayList<Person> personPerDocument = new ArrayList<Person>();
+    private final ArrayList<Node> nodePerDocument = new ArrayList<Node>();
+    private static Graph graph;
 
-    public NameExtractor(Document doc) {
+    public NameExtractor(Document doc, Graph g) {
+        graph = g;
         try {
             String docContent = doc.getContent();
             extractNames(docContent);
@@ -136,6 +145,18 @@ class NameExtractor {
      */
     public ArrayList<Person> returnPersonArray() {
         return personPerDocument;
+    }
+
+    public ArrayList<Node> castPersonsToNodes(ArrayList<Person> al) {
+
+        int cnt = 0;
+        for(Person p : al){
+            Node node = graph.createNode(Integer.toString(cnt));
+            node.setLabel(p.getPrename() + " " + p.getLastname());
+            nodePerDocument.add(node);
+        }
+
+        return nodePerDocument;
     }
 
     /**

05e457a3f86b60807999ca90dc49f4b5988bddd5
IgorBosnjak <igor.bosnjak11@gmail.com>
2014-06-16 13:14:26 +0200

    added comments

--- src/main/java/ch/fhnw/coin/coolpeople/Window.java
+++ src/main/java/ch/fhnw/coin/coolpeople/Window.java
@@ -8,10 +8,8 @@ import java.awt.event.ActionListener;
 import java.util.*;
 
 class Window extends JFrame {
-    ArrayList<Person> nameList = Main.templist;
     private final JLabel deletedNames = new JLabel("deleted names");
     private final JLabel integratedNames = new JLabel("integrated names");
-    //Java 7 has no generics for JList (no new JList<String>();)
     private DefaultListModel<Person> deletedModel = new DefaultListModel<Person>();
     private final JList deletedNamesArea = new JList(deletedModel);
     private DefaultListModel<Person> integratedModel = new DefaultListModel<Person>();
@@ -19,22 +17,17 @@ class Window extends JFrame {
     private final JButton addName = new JButton( "->" );
     private final JButton deleteName = new JButton( "<-" );
     private JButton createNetwork = new JButton("save");
-    //Person[] personsInList = nameList.toArray(new Person[0]);
 
     public Window() {
         super("CoolPeople");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-
-        //for(int i =0; i<personsInList.length; i++) {
-        //String[] personNameList = personsInList.toString();
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
         this.setSize(1000, 600);
 
         panel.setName("CoolPeople");
-        //JFrame myFrame = new JFrame("CoolPeople");
         panel.add(getEastPanel(), BorderLayout.EAST);
         panel.add(getWestPanel(), BorderLayout.WEST);
         panel.add(getCenterPanel(), BorderLayout.CENTER);
@@ -44,6 +37,13 @@ class Window extends JFrame {
         setLocationRelativeTo(null);
         pack();
     }
+
+    /**
+     *
+     * GUI Layoutdefinition
+     *
+     * @return
+     */
 
     JComponent getCenterPanel() {
         JPanel inner = new JPanel();
@@ -79,6 +79,13 @@ class Window extends JFrame {
         return inner;
     }
 
+    /**
+     *
+     * List for fill the integrate persons
+     *
+     * @param list
+     */
+
     public void integratedpersonList(ArrayList<Person> list){
         for(int i = 0; i<list.size(); i++){
             integratedModel.addElement(list.get(i));
@@ -94,6 +101,10 @@ class Window extends JFrame {
 
         }
     }
+
+    /**
+     * ActionListener for the buttons
+     */
 
     public ActionListener deleteListener = new ActionListener() {
         @Override
@@ -129,54 +140,4 @@ class Window extends JFrame {
             integratedModel.remove(integratedNamesArea.getSelectedIndex());
         }
     };
-
-    /*
-    public int getSize() {
-        return model.size();
-    }
-
-    public Object getElementAt(int index) {
-        return model.toArray()[index];
-    }
-
-    public void add(Object element) {
-        if (model.add(element)) {
-            fireContentsChanged(this, 0, getSize());
-        }
-    }
-
-    public void addAll(Object elements[]) {
-        Collection<Object> c = Arrays.asList(elements);
-        model.addAll(c);
-        fireContentsChanged(this, 0, getSize());
-    }
-
-    public void clear() {
-        model.clear();
-        fireContentsChanged(this, 0, getSize());
-    }
-
-    public boolean contains(Object element) {
-        return model.contains(element);
-    }
-
-    public Object firstElement() {
-        return model.first();
-    }
-
-    public Iterator iterator() {
-        return model.iterator();
-    }
-
-    public Object lastElement() {
-        return model.last();
-    }
-
-    public boolean removeElement(Object element) {
-        boolean removed = model.remove(element);
-        if (removed) {
-            fireContentsChanged(this, 0, getSize());
-        }
-        return removed;
-    }*/
 }
